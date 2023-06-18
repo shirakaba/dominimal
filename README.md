215ff07ed528cbb9e9a63c697dd23ff3a523e792
shirakaba <14055146+shirakaba@users.noreply.github.com>
2023-06-18 11:44:36 +0900

    initial boilerplate

---
+++ README.md
@@ -0,0 +1,7 @@
+# dominimal
+
+A minimal subset of jsdom, focusing on DOM (and omitting wider Web APIs).
+
+```sh
+npm run build
+```
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+const isProduction = process.env.NODE_ENV == 'production';
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        loader: 'esbuild-loader',
+        options: {
+          target: 'es2015',
+        },
+      },
+
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = 'production';
+  } else {
+    config.mode = 'development';
+  }
+  return config;
+};
