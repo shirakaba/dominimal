2bb4e1577130ea447fb73a5c16279e7cc4dceba3
shirakaba <14055146+shirakaba@users.noreply.github.com>
2023-06-18 15:18:34 +0900

    working webpack config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+const { JSDOM } = require('jsdom');
+
+const jsdom = new JSDOM('', { pretendToBeVisual: true });
+const { document } = jsdom.window;
+const body = document.createElement('body');
+document.documentElement.appendChild(body);
+
+console.log(jsdom.serialize());
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,41 @@
+// const BundleAnalyzerPlugin =
+//   require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const { EsbuildPlugin } = require('esbuild-loader');
 const path = require('path');
 
 const isProduction = process.env.NODE_ENV == 'production';
+const esbuildOptions = { target: 'es2021' };
 
 const config = {
   entry: './src/index.js',
+  target: 'node',
   output: {
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
-    // Add your plugins here
+    // new BundleAnalyzerPlugin(),
     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
+  resolve: {
+    alias: {
+      canvas$: false,
+      bufferutil$: false,
+      'utf-8-validate$': false,
+    },
+  },
   module: {
     rules: [
       {
         test: /\.[jt]sx?$/,
         loader: 'esbuild-loader',
-        options: {
-          target: 'es2015',
-        },
+        options: esbuildOptions,
       },
 
       // Learn more about loaders from https://webpack.js.org/loaders/
     ],
+  },
+  optimization: {
+    minimizer: [new EsbuildPlugin(esbuildOptions)],
   },
 };
 
